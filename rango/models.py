d868bca2965f930fef442a8824927a25b5b3d21a
nandini <nandini.soni8@gmail.com>
2016-11-05 09:42:32 +0530

    first commit for kpi models

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils import timezone
 
 # Create your models here.
 
@@ -27,6 +28,45 @@ class Page(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Metrics(models.Model):
+	metric_name = models.CharField(max_length=50)
+	metric_type = models.CharField(max_length=10)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	row_status = models.IntegerField(blank=False)
+
+	def __str__(self):
+		return self.metric_name
+	
+
+class MetricData(models.Model):
+	dim_1 = models.CharField(max_length=50)
+	dim_2 = models.CharField(max_length=50)
+	dim_3 = models.CharField(max_length=50)
+	attr_1 = models.CharField(max_length=50)
+	attr_2 = models.CharField(max_length=50)
+	attr_3 = models.CharField(max_length=50)
+	date_associated = models.DateField()
+	metric_id = models.ForeignKey(Metrics)
+	numerator = models.IntegerField()
+	denominator = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	row_status = models.IntegerField(blank=False)
+	loaded_by = models.CharField(max_length=50)
+
+
+class Dimension(models.Model):
+	dimension = models.CharField(max_length=50)
+	level = models.IntegerField(blank=False)
+	dim_name = models.CharField(max_length=50)
+	parent_id = models.IntegerField(blank=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.dimension
 
 
 
